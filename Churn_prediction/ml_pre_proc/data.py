e013908670a5402427f6bbf9ead45734aca5bed2
Andre Tomaz <andrereinotomaz@hotmail.com>
2023-09-18 20:52:22 +0100

    model packaging and main updated

--- Churn_prediction/ml_pre_proc/data.py
+++ Churn_prediction/ml_pre_proc/data.py
@@ -40,35 +40,3 @@ def get_data(
 
     return members_v3, transactions, transactions_v2, user_logs_v2, train, train_v2
 
-def load_data_to_bq(
-        data: pd.DataFrame,
-        gcp_project:str,
-        bq_dataset:str,
-        table: str,
-        truncate: bool
-    ) -> None:
-    """
-    - Save the DataFrame to BigQuery
-    - Empty the table beforehand if `truncate` is True, append otherwise
-    """
-
-    assert isinstance(data, pd.DataFrame)
-    full_table_name = f"{gcp_project}.{bq_dataset}.{table}"
-    print(Fore.BLUE + f"\nSave data to BigQuery @ {full_table_name}...:" + Style.RESET_ALL)
-
-
-    data.columns = [f"_{column}" if not str(column)[0].isalpha() and not str(column)[0] == "_" else str(column) for column in data.columns]
-
-    client = bigquery.Client()
-
-    # Define write mode and schema
-    write_mode = "WRITE_TRUNCATE" if truncate else "WRITE_APPEND"
-    job_config = bigquery.LoadJobConfig(write_disposition=write_mode)
-
-    print(f"\n{'Write' if truncate else 'Append'} {full_table_name} ({data.shape[0]} rows)")
-
-    # Load data
-    job = client.load_table_from_dataframe(data, full_table_name, job_config=job_config)
-    result = job.result()  # wait for the job to complete
-
-    print(f"✅ Data saved to bigquery, with shape {data.shape}")
