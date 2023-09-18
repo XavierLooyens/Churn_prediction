d8359ca5d97bd2d99e4eece406fd6268a8ea2e9f
Andre Tomaz <andrereinotomaz@hotmail.com>
2023-09-18 17:10:18 +0100

    updated main file and importing data file

--- Churn_prediction/ml_pre_proc/data.py
+++ Churn_prediction/ml_pre_proc/data.py
@@ -1,35 +1,44 @@
 import pandas as pd
 from google.cloud import bigquery
-from pathlib import Path
-
 
 def get_data(
         gcp_project:str,
-        query:str,
-        cache_path:Path,
         data_has_header=True
     ) -> pd.DataFrame:
-    """
-    Retrieve `query` data from BigQuery, or from `cache_path` if the file exists
-    Store at `cache_path` if retrieved from BigQuery for future use
-    """
-    if cache_path.is_file():
-        print(Fore.BLUE + "\nLoad data from local CSV..." + Style.RESET_ALL)
-        df = pd.read_csv(cache_path, header='infer' if data_has_header else None)
-    else:
-        print(Fore.BLUE + "\nLoad data from BigQuery server..." + Style.RESET_ALL)
-        client = bigquery.Client(project=gcp_project)
-        query_job = client.query(query)
-        result = query_job.result()
-        df = result.to_dataframe()
 
-        # Store as CSV if the BQ query returned at least one valid line
-        if df.shape[0] > 1:
-            df.to_csv(cache_path, header=data_has_header, index=False)
+    client = bigquery.Client(project=gcp_project)
 
-    print(f"✅ Data loaded, with shape {df.shape}")
+    query = """SELECT * FROM churn-prediction-398917.churn_prediction.members_v3"""
+    query_job = client.query(query)
+    result = query_job.result()
+    members_v3= result.to_dataframe()
 
-    return df
+    query = """SELECT * FROM churn-prediction-398917.churn_prediction.transactions"""
+    query_job = client.query(query)
+    result = query_job.result()
+    transactions = result.to_dataframe()
+
+    query = """SELECT * FROM churn-prediction-398917.churn_prediction.transactions"""
+    query_job = client.query(query)
+    result = query_job.result()
+    transactions_v2 = result.to_dataframe()
+
+    query = """SELECT * FROM churn-prediction-398917.churn_prediction.user_logs_v2"""
+    query_job = client.query(query)
+    result = query_job.result()
+    user_logs_v2 = result.to_dataframe()
+
+    query = """SELECT * FROM churn-prediction-398917.churn_prediction.train"""
+    query_job = client.query(query)
+    result = query_job.result()
+    train = result.to_dataframe()
+
+    query = """SELECT * FROM churn-prediction-398917.churn_prediction.train_v2"""
+    query_job = client.query(query)
+    result = query_job.result()
+    train_v2 = result.to_dataframe()
+
+    return members_v3, transactions, transactions_v2, user_logs_v2, train, train_v2
 
 def load_data_to_bq(
         data: pd.DataFrame,
@@ -47,15 +56,7 @@ def load_data_to_bq(
     full_table_name = f"{gcp_project}.{bq_dataset}.{table}"
     print(Fore.BLUE + f"\nSave data to BigQuery @ {full_table_name}...:" + Style.RESET_ALL)
 
-    # Load data onto full_table_name
 
-    # 🎯 HINT for "*** TypeError: expected bytes, int found":
-    # After preprocessing the data, your original column names are gone (print it to check),
-    # so ensure that your column names are *strings* that start with either
-    # a *letter* or an *underscore*, as BQ does not accept anything else
-
-    # TODO: simplify this solution if possible, but students may very well choose another way to do it
-    # We don't test directly against their own BQ tables, but only the result of their query
     data.columns = [f"_{column}" if not str(column)[0].isalpha() and not str(column)[0] == "_" else str(column) for column in data.columns]
 
     client = bigquery.Client()
